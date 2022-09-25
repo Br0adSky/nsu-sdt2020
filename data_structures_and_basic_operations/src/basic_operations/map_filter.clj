d0d88a95f3b67eb0ce6f6bf5b860f4456cf6c94d
shift-lab-zozulia <artem.z19.06@gmail.com>
2022-09-25 18:19:21 +0700

    Лабораторная работа 1 Реализация ЛР 1.3, 1.4

---
+++ data_structures_and_basic_operations/src/basic_operations/map_filter.clj
@@ -0,0 +1,21 @@
+(ns basic-operations.map_filter)
+(def alphabet '(:a :b :c))
+
+(defn add-new-word
+  "Создает список новых слов из переданного слова"
+  [letters word]
+  (map #(cons % word) letters))
+
+(defn add-alphabet-to-word
+  "Создает список возможных слов"
+  [word]
+  (add-new-word (filter #(not (= (first word) %)) alphabet) word))
+
+(defn increment-words
+  "Возвращает список слов, увеличенных на алфавит"
+  [words]
+  (apply concat (map #(add-alphabet-to-word %) words)))
+
+(defn iterate-words
+  "Увеличивает число слов, пока длина не станет = n"
+  [n] (nth (iterate increment-words (add-new-word alphabet (list))) (dec n)))

12e3017bdf13ac960a29c3d6abbb3c6228a09d74
shift-lab-zozulia <artem.z19.06@gmail.com>
2022-11-09 23:33:56 +0700

    Лабораторная работа 2 правки МР

--- data_structures_and_basic_operations/src/integral/core.clj
+++ data_structures_and_basic_operations/src/integral/core.clj
@@ -2,18 +2,15 @@
 (defn one-trapeze [start finish step f acc]
   (+ acc (* step (/ (+ (f start) (f finish)) 2))))
 
-(def ^:private memo-trapeze
-  (memoize one-trapeze))
-
 (defn count-integral [acc current step f integral-func]
   (integral-func (- current step) current step f acc))
-
-(defn integral [finish f step]
-  (reduce (fn [acc a] (count-integral acc a step f memo-trapeze))
-          (range 0.0 (+ finish step) step)))
-
 (def my-func
   (fn [x]
     (Thread/sleep 1)
     (* x 2)))
+
+(defn integral [f step]
+  (let [memo-trapeze (memoize one-trapeze)]
+    (fn [finish] (reduce (fn [acc a] (count-integral acc a step f memo-trapeze))
+                         (range 0.0 (+ finish step) step)))))
 
